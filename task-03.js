02ddd155af68d71e51a48a58ab94bf949e2011ce
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-01-23 15:29:19 +0200

    fix pretty

--- task-03.js
+++ task-03.js
@@ -3,8 +3,8 @@
 const findLongestWord = function(string) {
   const words = string.split(" ");
   let longestWord = words[0];
-  
-  for (let i = 1; i < words.length; i ++) {
+
+  for (let i = 1; i < words.length; i++) {
     if (words[i].length > longestWord.length) {
       longestWord = words[i];
     }
--- task-06.js
+++ task-06.js
@@ -3,28 +3,26 @@
 let input;
 const numbers = [];
 let total = 0;
-while(true){
-input = prompt('Введите любое число:');
-if (input === null){
-    alert('Отменено пользователем!')
-    break;   
-}
-input = Number(input);
+while (true) {
+  input = prompt("Введите любое число:");
+  if (input === null) {
+    alert("Отменено пользователем!");
+    break;
+  }
+  input = Number(input);
   if (Number.isNaN(input)) {
     alert("Было введено не число, попробуйте еще раз");
     continue;
-
   } else {
-      numbers.push((input));
+    numbers.push(input);
   }
 }
 
-for (let i =0; i < numbers.length; i++) {
-    total = total + numbers[i];
+for (let i = 0; i < numbers.length; i++) {
+  total = total + numbers[i];
 }
 console.log(numbers);
 alert(`Общая сумма чисел равна ${total}`);
-
 
 // 1)Пользователь вводит любое число
 // 2)Сохраняет в переменную это число
